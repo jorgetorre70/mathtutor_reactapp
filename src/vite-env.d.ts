9f21ff6e22bc8f4f3378968d908d920fdfd521d5
jorgetorre70 <jorgetorre1022@gmail.com>
2024-10-29 11:28:19 -0600

    Modifcations for deployment on AWS Amplify

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_GPT_API: string;
+  readonly VITE_ASSISTANT_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.VITE_GPT_API": JSON.stringify(process.env.VITE_GPT_API),
+    "process.env.VITE_ASSISTANT_API": JSON.stringify(
+      process.env.VITE_ASSISTANT_API
+    ),
+  },
+  build: {
+    sourcemap: true,
+    outDir: "dist",
+  },
+});
